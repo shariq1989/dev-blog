4e8e950b4f59ff2f58d6f655517119586d30e426
Rick J <shariq1989@gmail.com>
2021-03-15 23:02:28 -0600

    update script

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 # build site
+echo "------Building site------------------"
 hugo -D
 # wipe previous version of site
+echo "------Deleting previous version------"
 rm -rf ../../shariq1989.github.io/*
 # copy over new version
-cp -r ./public/* ../../shariq1989.github.io/*
+echo "------Copying over new version-------"
+cp -r ./public/* ../../shariq1989.github.io/
 # push changes
+echo "------Pushing changes----------------"
 cd ../../shariq1989.github.io
 git add *
 git commit -m "new version"
